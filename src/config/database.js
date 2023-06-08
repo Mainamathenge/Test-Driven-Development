c9363607d94810bcf9f47ac8a649da8124f4023c
Mainamathenge <mainamathengej@gmail.com>
2023-06-08 08:20:33 +0300

    test

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-pass', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+const config = require('config');
+
+const dbconfig = config.get('database');
+
+const sequelize = new Sequelize(dbconfig.database, dbconfig.username, dbconfig.password, {
+  dialect: dbconfig.dialect,
+  storage: dbconfig.storage,
+  logging: dbconfig.logging,
 });
 
 module.exports = sequelize;
